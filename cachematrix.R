ccee95f241796a32d45b9aea9a34574a01745e4b
pasmikh <pasmikh@gmail.com>
2016-01-08 00:22:12 +0300

    Functions inserted

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeMatrix(x) creates a special matrix, that caches an inverse matrix, then 
+#cachesolve(x) is used to output cached inverse matrix or count one
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#makeMatrix makes special matrix
+makeMatrix <- function(x = matrix()) {
+      s <- NULL
+      set <- function(y) {
+            x <<- y
+            s <<- NULL
+      }
+      get <- function() x
+      setsolve <- function(solve) s <<- solve
+      getsolve <- function() s
+      list(set = set, get = get,
+           setsolve = setsolve,
+           getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      s <- x$getsolve()
+      if(!is.null(s)) {
+            message("getting cached data")
+            return(s) #returns cached matrix
+      }
+      data <- x$get()
+      s <- solve(data, ...) #solves the matrix
+      x$setsolve(s) #saves solved matrix to the special matrix
+      s #returns solved matrix
 }
